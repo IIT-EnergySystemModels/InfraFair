8c753f8c2ef938a60ca090a8f27264b83f76ff0f
MohElabbas <89938487+MohElabbas@users.noreply.github.com>
2024-07-07 19:42:24 +0200

    Update __init__.py

--- InfraFair/__init__.py
+++ InfraFair/__init__.py
@@ -4,6 +4,6 @@ InfraFair.
 'Fairness in allocating infrastructure cost'
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = 'Mohamed A.Eltahir Elabbas'
 __credits__ = 'Universidad Pontificia Comillas'
